0d7341d74148ed883308fb3097b0c124e96a088d
whit33y <troppout@gmail.com>
2025-01-12 14:49:46 +0100

    add complete and delete task functions

--- src/app/components/tasks/task/task.component.ts
+++ src/app/components/tasks/task/task.component.ts
@@ -1,12 +1,26 @@
 import { Component, Input } from '@angular/core';
+import { TasksService } from '../../../services/tasks.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
 export class TaskComponent {
   @Input() task: string = '';
+  @Input() id: number | undefined = undefined;
+  @Input() status: boolean = false;
+
+  constructor(private tasksService: TasksService) {}
+
+  completeTask(id: number) {
+    this.tasksService.completeTask(id);
+  }
+
+  deleteTask(id: number) {
+    this.tasksService.deleteTask(id);
+  }
 }
