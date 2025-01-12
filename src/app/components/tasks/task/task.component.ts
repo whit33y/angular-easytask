907180cd6c65646e9681d26637f0a4fb0b7a0355
whit33y <troppout@gmail.com>
2025-01-12 14:19:49 +0100

    move task code to task component

--- src/app/components/tasks/task/task.component.ts
+++ src/app/components/tasks/task/task.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-task',
   standalone: true,
   imports: [],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.css'
+  styleUrl: './task.component.css',
 })
 export class TaskComponent {
-
+  @Input() task: string = '';
 }
